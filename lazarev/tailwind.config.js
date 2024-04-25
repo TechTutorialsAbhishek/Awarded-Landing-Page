5dceb7998b79b9ad6ac0024a862c357ad788fcc0
Abhishek Kumar <abhishek9661342993@gmail.com>
2024-04-26 02:18:51 +0530

    add new Landing page that needed to Modifie

---
+++ lazarev/tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*'],
+  theme: {
+    extend: {
+      colors: {
+        'regal-blue': '#243c5a',
+      },
+     
+        height: {
+          '128': '32rem',
+        },
+    },
+  },
+  plugins: [],
+}
+
